8bad4c3ee19db9df052aa9bcff613c2c1a39c085
pupungeggang6 <pupungeggang6@gmail.com>
2024-10-01 21:24:46 +0900

    Start

---
+++ Game/Script/FunctionDraw.js
@@ -0,0 +1,11 @@
+function drawSceneInit() {
+    context.font = '32px neodgm'
+    context.textAlign = 'left'
+    context.textBaseline = 'top'
+    context.fillStyle = 'White'
+    context.strokeStyle = 'Black'
+    context.lineWidth = 2
+    context.clearRect(0, 0, 1280, 720)
+    context.fillRect(0, 0, 1280, 720)
+    context.fillStyle = 'Black'
+}
---
+++ Game/Script/Var.js
@@ -0,0 +1,10 @@
+let canvas
+let context
+
+let gameInstance
+let gameFrameCurrent
+let gameFramePrevious
+let delta
+
+let scene = 'Title'
+let state = ''
